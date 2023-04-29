b77c402ab1165ce61d002cf6310550a1b875117e
Ei3-kw <52182320+Ei3-kw@users.noreply.github.com>
2023-04-29 11:34:24 +1000

    3c

--- archieve/2023-3/c.py
+++ archieve/2023-3/c.py
@@ -6,62 +6,43 @@ class Book:
         self.no = no
         self.l = l
         self.d = d
+
+    def include(self, books):
+        for b in books:
+            if b.no == self.no:
+                return True
+        return False
     
-    def __lt__(self, o):
-        return self.l/self.d <= o.l/o.d
-
-    def __str__(self):
-        return str([self.no, self.l, self.d])
-
-class B(Book):
     def __lt__(self, o):
         return self.d <= o.d
 
 
-n, m = input().split()
+class B(Book):
+    def __lt__(self, o):
+        return self.l/self.d <= o.l/o.d
+    
 
-n = int(n)
-m = int(m)
+n, m = map(int, input().split())
 
 books = []
 
 for i in range(n):
-    l, d = input().split()
-    books.append(Book(i, int(l), int(d)))
-
+    l, d = map(int, input().split())
+    books.append(Book(i, l, d))
 books.sort()
-books = books[:n-m]
 
+p = 0
 bs = []
 for b in books:
-    bs.append(B(b.no, b.l, b.d))
+    p += b.l
+    bs.append(B(b.no, p, b.d))
 bs.sort()
+bs = bs[n-m:]
 
-# lower = 0
-# x0 = x = 10**4/2
-# upper = 10**4
-# while 1:
-#     x0 = x
-#     p = 0
-#     for b in books:
-#         p += b.l
-#         if b.d * x < p:
-#             lower = x
-            
-#         else:
-#             upper = x
-#         x = int((upper+lower)/2)
-#     if x == x0:
-#         break
-
-x = 0
-p = 0
-for b in bs:
+x = p = 0
+for b in filter(lambda b: b.include(bs), books):
     p += b.l
     x = max(x, p/b.d)
 
 print(math.ceil(x))
-        
 
-
-            
